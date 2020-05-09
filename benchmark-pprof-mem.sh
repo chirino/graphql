1b9278f34110982d0364bb6ecf074566cb778aaa
Hiram Chirino <hiram@hiramchirino.com>
2020-05-09 03:00:24 -0400

    chore: making the benchmark script more flexible.

--- benchmark-pprof-mem.sh
+++ benchmark-pprof-mem.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 mkdir benchmark-results 2> /dev/null
-go test -bench="$1" -benchmem -memprofile benchmark-results/latest.mem > benchmark-results/latest.txt
+go test -c -o benchmark-results/test
+
+#export GODEBUG=allocfreetrace=1,gctrace=1
+#BENCHMARK=BenchmarkParallelParseStarwarsQuery
+BENCHMARK=${1:-BenchmarkParallelParseStarwarsQuery}
+
+./benchmark-results/test -test.bench=$BENCHMARK -test.benchmem -test.memprofile test.benchmark-results/latest.mem  | tee b benchmark-results/latest-memprofile.txt
+
 go tool pprof -alloc_objects -lines -top benchmark-results/latest.mem | head -n 25
 go tool pprof -alloc_space -lines -top benchmark-results/latest.mem | head -n 25
--- benchmark.sh
+++ benchmark.sh
@@ -3,12 +3,26 @@
 mkdir benchmark-results 2> /dev/null
 mv benchmark-results/latest.txt benchmark-results/previous.txt 2> /dev/null
 
+go test -c -o benchmark-results/test
 
-go test -bench=. > benchmark-results/latest.txt
+#export GODEBUG=allocfreetrace=1,gctrace=1
+#BENCHMARK=BenchmarkParallelParseStarwarsQuery
+BENCHMARK=${1:-.}
+
+results() {
+    if [[  "" == "$1" ]] ; then
+        benchstat benchmark-results/previous.txt benchmark-results/latest.txt
+    else
+        benchstat "$1" benchmark-results/latest.txt
+    fi
+}
+
+./benchmark-results/test  -test.bench=$BENCHMARK | tee benchmark-results/latest.txt
+results $1
+./benchmark-results/test  -test.bench=$BENCHMARK | tee -a benchmark-results/latest.txt
+results $1
+./benchmark-results/test  -test.bench=$BENCHMARK | tee -a benchmark-results/latest.txt
+results $1
+
 cp "benchmark-results/latest.txt" "benchmark-results/$(date).txt"
 
-if [[  "" == "$1" ]] ; then
-    benchstat benchmark-results/previous.txt benchmark-results/latest.txt
-else
-    benchstat "$1" benchmark-results/latest.txt
-fi
