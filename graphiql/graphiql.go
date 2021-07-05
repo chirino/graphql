09c3faea9ea31f68d52caedd9587aa9cf2e31d74
Hiram Chirino <hiram@hiramchirino.com>
2021-07-05 09:01:09 -0400

    feat: simplify graphiql html and enable header editing

--- graphiql/graphiql.go
+++ graphiql/graphiql.go
@@ -16,142 +16,44 @@ type Handler struct {
 
 func New(urlPath string, ws bool) *Handler {
 	html := `
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8" />
-  <meta name="robots" content="noindex" />
-  <meta name="referrer" content="origin" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>SWAPI GraphQL API</title>
+  <title>GraphiQL</title>
   <style>
-    body {
-      height: 100vh;
-      margin: 0;
-      overflow: hidden;
-    }
-    #splash {
-      color: #333;
-      display: flex;
-      flex-direction: column;
-      font-family: system, -apple-system, "San Francisco", ".SFNSDisplay-Regular", "Segoe UI", Segoe, "Segoe WP", "Helvetica Neue", helvetica, "Lucida Grande", arial, sans-serif;
-      height: 100vh;
-      justify-content: center;
-      text-align: center;
-    }
+      body {
+          height: 100%;
+          margin: 0;
+          width: 100%;
+          overflow: hidden;
+      }
+      #graphiql {
+          height: 100vh;
+      }
   </style>
-  <link rel="icon" href="favicon.ico">
-  <link type="text/css" href="//unpkg.com/graphiql@0.17.0/graphiql.css" rel="stylesheet" />
+  <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
+  <link href="https://unpkg.com/graphiql/graphiql.min.css" rel="stylesheet" />
+  <script crossorigin src="https://unpkg.com/graphiql/graphiql.min.js"></script>
 </head>
 <body>
-  <div id="splash">
-    Loading&hellip;
-  </div>
-  <script src="//cdn.jsdelivr.net/es6-promise/4.0.5/es6-promise.auto.min.js"></script>
-  <script src="//cdn.jsdelivr.net/react/15.4.2/react.min.js"></script>
-  <script src="//cdn.jsdelivr.net/react/15.4.2/react-dom.min.js"></script>
-  <script src="//unpkg.com/graphiql@0.17.0/graphiql.min.js"></script>
-{{ if .WebSocket }}
-  <script src="//unpkg.com/subscriptions-transport-ws@0.9.16/browser/client.js"></script>
-  <script src="//unpkg.com/graphiql-subscriptions-fetcher@0.0.2/browser/client.js"></script>
-{{ end }}
-
+  <div id="graphiql">Loading...</div>
   <script>
-      // Parse the search string to get url parameters.
-      var search = window.location.search;
-      var parameters = {};
-      search.substr(1).split('&').forEach(function (entry) {
-        var eq = entry.indexOf('=');
-        if (eq >= 0) {
-          parameters[decodeURIComponent(entry.slice(0, eq))] =
-            decodeURIComponent(entry.slice(eq + 1));
-        }
-      });
-
-      // if variables was provided, try to format it.
-      if (parameters.variables) {
-        try {
-          parameters.variables =
-            JSON.stringify(JSON.parse(parameters.variables), null, 2);
-        } catch (e) {
-          // Do nothing, we want to display the invalid JSON as a string, rather
-          // than present an error.
-        }
-      }
-
-      // When the query and variables string is edited, update the URL bar so
-      // that it can be easily shared
-      function onEditQuery(newQuery) {
-        parameters.query = newQuery;
-        updateURL();
-      }
-      function onEditVariables(newVariables) {
-        parameters.variables = newVariables;
-        updateURL();
-      }
-      function onEditOperationName(newOperationName) {
-        parameters.operationName = newOperationName;
-        updateURL();
-      }
-      function updateURL() {
-        var newSearch = '?' + Object.keys(parameters).filter(function (key) {
-          return Boolean(parameters[key]);
-        }).map(function (key) {
-          return encodeURIComponent(key) + '=' +
-            encodeURIComponent(parameters[key]);
-        }).join('&');
-        history.replaceState(null, null, newSearch);
-      }
-
-{{ if .WebSocket }}
-      let subscriptionsClient = new window.SubscriptionsTransportWs.SubscriptionClient('{{.URL}}', {
-        reconnect: true
-      });
-      
-      function graphQLFetcher(graphQLParams) {
-        return subscriptionsClient.request(graphQLParams);
-
-      };
-{{ else }}
-
-      // Defines a GraphQL fetcher using the fetch API. You're not required to
-      // use fetch, and could instead implement graphQLFetcher however you like,
-      // as long as it returns a Promise or Observable.
-      function graphQLFetcher(graphQLParams) {
-          // This example expects a GraphQL server at the path /graphql.
-          // Change this to point wherever you host your GraphQL server.
-          return fetch('{{.URL}}', {
-              method: 'post',
-              headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(graphQLParams),
-          }).then(function (response) {
-              return response.text();
-          }).then(function (responseBody) {
-              try {
-                  return JSON.parse(responseBody);
-              } catch (error) {
-                  return responseBody;
-              }
-          });
-      }
-{{ end }}
-
-      // Render <GraphiQL /> into the body.
-      ReactDOM.render(
-        React.createElement(GraphiQL, {
-          fetcher: graphQLFetcher,
-          query: parameters.query,
-          variables: parameters.variables,
-          operationName: parameters.operationName,
-          onEditQuery: onEditQuery,
-          onEditVariables: onEditVariables,
-          onEditOperationName: onEditOperationName
-        }),
-        document.body,
-      );
+  {{- if .WebSocket }}
+    let subscriptionsClient = new window.SubscriptionsTransportWs.SubscriptionClient('{{.URL}}', {
+  	reconnect: true
+    }); 
+    let fetcher = subscriptionsClient.request;
+  {{- else }}
+    let fetcher = GraphiQL.createFetcher({url: '{{.URL}}'})
+  {{- end }}
+    ReactDOM.render(
+  	  React.createElement(GraphiQL, { 
+  	  	fetcher: fetcher,
+  	    headerEditorEnabled: true,
+  	  	shouldPersistHeaders: true,	
+  	  }),
+  	  document.getElementById('graphiql'),
+    );
   </script>
 </body>
 </html>
